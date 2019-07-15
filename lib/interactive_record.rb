8db8f58b3e4056093bc9cf86f310dc8b5c962b39
Sara Harvey <saraharveynyc@gmail.com>
2019-07-15 18:29:49 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -1,9 +1,10 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
-class InteractiveRecord
-  def self.table_name
-    self.to_s.downcase.pluralize
+  def initialize(objects={})
+    objects.each do |k, v|
+      self.send("#{k}=", v)
+    end
   end
   
   
